b2cbb457975757f43acad895e06b08aee307167f
sofasurfer <kilian@sofasurfer.ch>
2019-02-21 13:37:54 +0100

    Fix video col padding

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,6 @@
     <meta name="author" content="<?= $site->author()->html() ?>">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">   
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-    <?= css('assets/css/index.css') ?>
+    <?= css('assets/css/index.css?v=1') ?>
 
 </head>
